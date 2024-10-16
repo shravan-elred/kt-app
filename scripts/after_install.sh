0253145d30a85a421b9b5186b50ee72ff86ba6bc
raahel <raahel@elred.io>
2024-10-16 09:47:15 +0530

    cicd: load nvm into current shell & add express to dependencies

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/kt-app
 
-pm2 stop kt-app
+pm2 stop app.js
 npm install
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/kt-app
 
-echo "Starting kt-app!"
-pm2 start app.js --name kt-app
+pm2 start app.js
