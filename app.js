9a7ce2dcf34e12e811e808dc6e12944fa96a676a
Shravan Chilka <shravanchilka16@gmail.com>
2024-10-22 18:57:43 +0530

    added /hello endpoint

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ app.get('/shravan', (req, res) => {
     res.send('Ahoy, This is Shravan!');
 });
 
-
 app.get('/raahel', (req, res) => {
     res.send('This is the best endpoint in the world');
 });
 
 app.get('/hello', (req, res) => {
     res.send('Hello');
+});
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
